bdb715e17993a376a5b535554bd9a37ded3283ec
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-07-27 18:12:28 -0500

    Objetos e interfaces

---
+++ src/bases/02-objects.ts
@@ -0,0 +1,26 @@
+
+export const pokemonIds = [1,20,30,34,66];
+
+// conversión a un número
+pokemonIds.push( +'1' );
+
+console.log(pokemonIds);
+
+interface Pokemon {
+    id: number;
+    name: string;
+    age: number;
+}
+
+export const bulbasaur: Pokemon = {
+    id: 1,
+    name: 'Bulbasaur',
+    age: 2
+}
+export const charmander: Pokemon = {
+    id: 1,
+    name: 'Charmander',
+    age: 1
+}
+
+console.log(bulbasaur)
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import typescriptLogo from './typescript.svg'
 import { setupCounter } from './counter'
 
 import { age, name } from './bases/01-types';
+import { bulbasaur, pokemonIds } from './bases/02-objects';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -13,6 +14,8 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript + ${ name } ${ age }</h1>
+    <h1>${ pokemonIds.join(',') }</h1>
+    <h1>${ bulbasaur.name }</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
